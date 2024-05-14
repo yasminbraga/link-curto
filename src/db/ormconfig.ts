05b34dd2c2d4446426502613036a6435e435007b
yasminbraga <yasminbragateixeira@gmail.com>
2024-05-14 18:27:18 -0300

    fix ormconfig

--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -5,17 +5,17 @@ export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.PG_HOST || 'localhost',
   port: +process.env.PG_PORT || 5432,
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_NAME,
+  username: process.env.PG_USER || 'link_curto',
+  password: process.env.PG_PASSWORD || 'link_curto',
+  database: process.env.PG_NAME || 'link_curto',
   synchronize: false,
   logging: true,
-  entities: ['src/**/*.entity{.ts,.js}'],
+  entities: [resolve(__dirname, 'src/**/*.entity{.ts,.js}')],
   migrations: [resolve(__dirname, 'src/db/migrations/*{.ts,.js}')],
-  migrationsTableName: 'links',
+  migrationsTableName: 'migrations',
   migrationsRun: true,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
-
+console.log(process.env.PG_USER);
 export default dataSource;
