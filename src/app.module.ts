98ff39eaa1b1e9c734a8fe42cf18524e5d00a480
yasminbraga <yasminbragateixeira@gmail.com>
2024-05-15 12:12:30 -0300

    remove migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { dataSourceOptions } from './db/ormconfig';
+import { resolve } from 'path';
 import { LinksModule } from './links/links.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), LinksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.PG_HOST || 'localhost',
+        port: +process.env.PG_PORT || 5432,
+        username: process.env.PG_USER || 'link_curto',
+        password: process.env.PG_PASSWORD || 'link_curto',
+        database: process.env.PG_NAME || 'link_curto',
+        logging: true,
+        entities: [resolve(__dirname, 'src/**/*.entity{.ts,.js}')],
+        autoLoadEntities: process.env.RUN_MIGRATIONS === 'true',
+        synchronize: process.env.RUN_MIGRATIONS === 'true',
+      }),
+    }),
+    LinksModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/links/link.entity.ts
+++ src/links/link.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('links')
 export class Link {
   @PrimaryGeneratedColumn()
   id: number;
